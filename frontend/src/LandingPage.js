2b9af928450c3e1430dde7c836db130334994d34
Eduardo <dumartins1009@gmail.com>
2023-01-21 23:36:35 -0500

    Updated first page of landing page

--- frontend/src/LandingPage.js
+++ frontend/src/LandingPage.js
@@ -5,8 +5,10 @@ import AboutTheProject from "./components/About.js";
 import Cards from "./components/Card.js"
 import What from "./components/What.js"
 import Stats from "./components/Stats.js"
+import ClickMee from "./components/Button.js"
 import FadeInSection from "./components/Fade.js"
 import Map from "./assets/map.jpg"
+import buildings from './assets/buildings.webp';
 import Ammar from "./assets/ammr.jpg"
 import Brett from "./assets/byang.jpg"
 import Eduardo from "./assets/Z.jpg"
@@ -19,20 +21,25 @@ function LandingPage() {
     <div
         className="container-fluid px-0"
       >
-          <div className="row" id="project-title" style={ {height: "400px", backgroundColor: "#121212"} }>
+        <div id="first-page" style={{backgroundColor: "#1f1e1e"}}>
+          <div className="row" id="project-title" style={ {height: "70vh"} }>
             <div className="col-sm">
               <h2 class="text-center" style={ {fontSize: "80px", color: "white",
-                backgroundSize: "100%", fontFamily: "Oswald", paddingTop: "9%"} }>
+                backgroundSize: "100%", fontFamily: "Oswald", paddingTop: "15%"} }>
                   RESCUE RANGER
               </h2>
               <p class="text-center" style= { {fontSize:"26px", fontFamily: "Oswald", color: "white", fontStyle:"italic"} }>
               Bringing hope to the front lines, one rover at a time. 
               </p>
             </div>
+            
           </div>  
-
-          <div style = {{ height: "4px", backgroundColor: "white"}}></div>
-          <div className="row" id="about-the-project" style={ {height: "700px", backgroundColor: "#121212"} }>
+          <div className="row" class="text-center" style= {{height: "20vh"}}>
+            <ClickMee />
+          </div>
+        </div>
+          <div style = {{ height: "4px", backgroundColor: "white"}}></div>  
+          <div id="tp" className="row" style={ {height: "100vh", backgroundColor: "#1f1e1e"} }>
             <div className="col-sm">
               <AboutTheProject />
             </div>
@@ -44,11 +51,11 @@ function LandingPage() {
 
           <FadeInSection>
           <div id="fade-in-section">
-            <div className="row" id="stats" style={ {paddingTop: "5%" , height: "200px", backgroundColor: "#121212", textAlign: "center"} }>
+            <div className="row" id="stats" style={ {paddingTop: "5%" , height: "200px", backgroundColor: "#1f1e1e", textAlign: "center"} }>
               <h1 style={{fontSize: "55px", color: "white", weight: "bold", fontFamily: "Oswald"}}>
                 How is Rescue Ranger relevant?</h1>
             </div>          
-            <div className="row" id="carousel" style={ {height: "600px", backgroundColor: "#121212"} }>
+            <div className="row" id="carousel" style={ {height: "600px", backgroundColor: "#1f1e1e"} }>
               <div style={{marginLeft:"20%", textAlign: "center", width: "60vw", height: "80vh"}}>
                 <ControlledCarousel />
               </div>
@@ -56,13 +63,13 @@ function LandingPage() {
           </div>
           </FadeInSection>
 
-          <div className="row" id="meet-the-team" style={ {height: "200px", backgroundColor: "#121212", textAlign: "center"} }>
+          <div className="row" id="meet-the-team" style={ {height: "200px", backgroundColor: "#1f1e1e", textAlign: "center"} }>
             <div style = {{height: "4px", backgroundColor: "white"}}></div>
             <h1 style={{fontSize: "55px", color: "white", fontFamily: "Oswald"}}>
               Meet the team behind Rescue Ranger </h1>
           </div>
 
-          <div className="row" style={ {paddingLeft:"3%" , textAlign: "center", height: "500px", backgroundColor: "#121212"} }>
+          <div className="row" style={ {paddingLeft:"3%" , textAlign: "center", height: "500px", backgroundColor: "#1f1e1e"} }>
             <div className="col-sm">
               <Cards name="Ammar Vora" desc="Second Year Computer Engineering" image={Ammar}/>
             </div>
---
+++ frontend/src/components/Button.js
@@ -0,0 +1,11 @@
+import Button from 'react-bootstrap/Button';
+
+function ClickMee() {
+  return (
+    <>
+      <Button href="#tp" style={{color: "white", fontFamily: "Oswald", fontSize: "25px", width:"200px", height:"80px", paddingTop:"20px"}} variant="outline-secondary" >Read More</Button>
+    </>
+  );
+}
+
+export default ClickMee;
