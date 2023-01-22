b22a13673f66b1ca6bb74ad8eea4f976136c2759
Ammar Vora <ammarmust4@gmail.com>
2023-01-21 19:07:25 -0500

    fixing merge conflicts

--- server/detect.py
+++ server/detect.py
@@ -26,7 +26,7 @@ while webcam.isOpened():
     # # Face detection
     t1 = time.time()
     faces, confidence = cvl.detect_face(img)
-    bbox, label, conf = cvl.detect_common_objects(img)
+    # bbox, label, conf = cvl.detect_common_objects(img)
     t2 = time.time()
     print(f'Inference at {1/(t2 - t1)} FPS')
 
@@ -35,18 +35,7 @@ while webcam.isOpened():
         x1, y1, x2, y2 = face
         out = cv2.rectangle(out, (x1, y1), (x2, y2), (255, 0, 0), 2)
 
-    bbox = np.array(bbox)
-    conf = np.array(conf)
-    label = np.array(label)
-    indx = [x for x in range(len(label)) if label[x] == 'person']
 
-    bbox = bbox[indx]
-    conf = conf[indx]
-    label = label[indx]
-    print(f'Detected {len(bbox)} people...')
-
-    out = draw_bbox(out, bbox, label, conf)
-
-    if len(bbox) + len(faces) > 0:
+    if len(faces) > 0:
         print('Saving...')
         cv2.imwrite('detected.png', out)
